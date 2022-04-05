c5e84ad2524d50dac9c5e54772e2e83074432bd4
Stanislav Pavlov <43157835+y2k01@users.noreply.github.com>
2022-04-05 09:40:06 +0900

    Fix About

--- pages/about.js
+++ pages/about.js
@@ -4,5 +4,5 @@ export default function AboutPage() {
             <Link href="/">Back to home!</Link>
         </div>
     </>
-  }
+}
   
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 export default function HomePage() {
-  return <div>Welcome to Next.js!
+  return <>
+    <h1>Welcome to next!</h1>
     <Link href="/about">To about!</Link>
-  </div>;
+  </>
 }
