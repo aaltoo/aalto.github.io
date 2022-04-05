8fb4bf65041bbed6ad0529c8340b292ebfce0543
Stanislav Pavlov <43157835+y2k01@users.noreply.github.com>
2022-04-05 09:33:23 +0900

    Add about page

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    return <>
+        <div>
+            <Link href="/">Back to home!</Link>
+        </div>
+    </>
+  }
+  
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
 export default function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+  return <div>Welcome to Next.js!
+    <Link href="/about">To about!</Link>
+  </div>;
 }
